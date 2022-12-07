d90f9415022660fa4089bf32ec641687f4502524
Andrew Xu <xusandrew@gmail.com>
2022-12-06 22:34:55 -0500

    Refactor grid creation function

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+const button = document.getElementById("button");
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16; i++){
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
-    
-    for (let j = 0; j < 16; j++){
-        let block = document.createElement("div");
-        block.classList.add("block");
 
-        block.addEventListener("mouseover", (e) => {
-            block.classList.add("hover");
-        });
+    for (let j = 0; j < size; j++) {
+      let block = document.createElement("div");
+      block.classList.add("block");
 
-        block.addEventListener("mouseout", (e) => {
-            block.classList.remove("hover");
-        });
+      block.addEventListener("mouseover", (e) => {
+        block.classList.add("hover");
+      });
 
-        row.appendChild(block);
+      block.addEventListener("mouseout", (e) => {
+        block.classList.remove("hover");
+      });
+
+      row.appendChild(block);
     }
     container.appendChild(row);
+  }
 }
